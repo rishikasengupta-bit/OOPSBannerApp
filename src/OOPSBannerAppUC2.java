7f549af001e2952d2d3fdabf4d849bc7d3676f2a
rishikasengupta-bit <pritha.sengupta20@gmail.com>
2026-02-17 10:39:54 +0530

    Coded to Render OOPS as Banner using Print Statements

--- src/OOPSBannerAppUC2.java
+++ src/OOPSBannerAppUC2.java
@@ -30,12 +30,12 @@ public class OOPSBannerAppUC2 {
     public static void main(String[] args) {
         
         System.out.println("  ***  " + " " + "  ***  " + " " + " ***** " + " " + " ***** ");
-        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** **" + " " + "***  **");
-        System.out.println("**   **" + " " + "**   **" + " " + " ** **" + " " + " *** ");
-        System.out.println("**   **" + " " + "**   **" + " " + " ***** " + " " + " **** ");
-        System.out.println("**   **" + " " + "**   **" + " " + " ** " + " " + "      *** ");
-        System.out.println(" ** ** " + " " + " ** ** " + " " + " ** " + " " + "**   ***");
-        System.out.println("  ***  " + " " + "  *** " + " " + "  ** " + " " + " *****  ");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " **  **" + " " + "***  **");
+        System.out.println("**   **" + " " + "**   **" + " " + " **  **" + " " + " *** ");
+        System.out.println("**   **" + " " + "**   **" + " " + " *****  " + " " + " **** ");
+        System.out.println("**   **" + " " + "**   **" + " " + " **  " + " " + "      *** ");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + " **  " + " " + "**   ***");
+        System.out.println("  ***  " + " " + "  *** " + " " + "  **  " + " " + " *****  ");
         
     }
 }
